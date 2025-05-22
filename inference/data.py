3297a81313151da12cf37988158ec99261043019
asztalosors <asztalos@reak.bme.hu>
2025-05-22 17:14:12 +0200

    #14 Adding verbose error message for incomapible datapoint input.

--- inference/data.py
+++ inference/data.py
@@ -5,9 +5,11 @@ Created on Wed May 21 15:06:08 2025
 @author: asztalos
 """
 
+import numpy as np
+
 class besInferenceDatapoints():
     
-    def __init__(self, grid, energy, species, zeff, temperature, q, ID, verbose=None, path=None):
+    def __init__(self, grid, energy, species, zeff, ID, temperature, q, verbose=None, path=None):
         if path is not None:
             self.__load_data_struct(path=path)
         
@@ -30,13 +32,22 @@ class besInferenceDatapoints():
         if len(density) == self.resolution and len(emission) == self.resolution:
             self.datapoints.append({'density': density, 'emission': emission, 'tag': tag})
         else:
-            raise ValueError('The input emission or density arrays do not match the size of the grid.')
+            raise ValueError('The input emission or density arrays do not match the size of the grid. Grid size: ' + 
+                             str(self.resolution) + ' Density size: ' + str(len(density)) + ' Emission size: ' + str(len(emission)))
     
     def add_datapoints_bulk(self, densities, emissions, tags):
-        pass
+        if densities.shape[1] != self.resolution or emissions.shape[1] != self.resolution:
+            raise ValueError('tbd')
+        elif densities.shape[0] != len(tags) or emissions.shape[0] != len(tags):
+            raise ValueError('TBD')
+        else:
+            for tag in range(len(tags)):
+                self.datapoints.append({'density': densities[tag, :],
+                                        'emission': emissions[tag, :],
+                                        'tag': tags[tag]})
     
     def get_datapoints(self):
-        pass
+        density, emission = np.array(())
     
     def export_to_h5(self):
         pass
