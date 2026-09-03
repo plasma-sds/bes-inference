2991164cddf7cae80d6f0db1c3935fb9178144a4
asztalosors <asztalos@reak.bme.hu>
2026-09-03 17:36:38 +0200

    #23 Adding setUp and tearDown routines, with 10 datapoints featruning resoltion of 50 points inlcluding errors.

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,9 +8,7 @@ Run with:
 """
 
 import unittest
-
 import numpy as np
-
 from neuro_bes.data import besInferenceDatapoints
 
 
@@ -18,11 +16,31 @@ class TestBesInferenceDatapoints(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures before each test method."""
-        pass
+        # Spatial grid of 50 points along the beam in [m].
+        self.resolution = 50
+        self.n_points = 10
+        self.grid = np.linspace(0.0, 1.0, self.resolution)
+
+        # Metadata for the object.
+        self.energy = 60
+        self.species = 'Li'
+        self.ID = 'test_dataset'
+        self.zeff = 1.5
+        self.q = 2.0
+        self.verbose = 'Dataset used for unit testing.'
+        self.temperature = np.full((self.n_points, self.resolution), 1.0e3)
+
+        # 10 density-emission pairs (50 points each) with matching errors.
+        rng = np.random.default_rng(seed=42)
+        self.densities = rng.uniform(1.0e18, 1.0e19, size=(self.n_points, self.resolution))
+        self.emissions = rng.uniform(0.0, 1.0, size=(self.n_points, self.resolution))
+        self.density_errors = 0.1 * self.densities
+        self.emission_errors = 0.1 * self.emissions
+        self.tags = ['point_' + str(i) for i in range(self.n_points)]
 
     def tearDown(self):
         """Clean up after each test method."""
-        pass
+        self.dataset = None
 
     # ------------------------------------------------------------------ #
     # __init__
